9487d6cb86176228625d2f0354c6bc12057b3f30
Akwon89 <a.soonkwon@gmail.com>
2016-03-21 14:50:38 -0400

    before bootstrap

--- sinatra-skeleton-master/app/models/track.rb
+++ sinatra-skeleton-master/app/models/track.rb
@@ -1,4 +1,12 @@
 class Track < ActiveRecord::Base
 
+  validates :song_title, presence: true, length: { maximum: 40 }
+  validates :author, presence: true, length: { maximum: 40 }
+  validate :url_checker, if: :url
+
+  def url_checker
+    errors.add(:invalid_url, ": url is invalid") unless url =~ /\A^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$\Z/i || url == ""
+  end
+
 
 end
